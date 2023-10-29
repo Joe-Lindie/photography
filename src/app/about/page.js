a1bad23eb9a3830a00f499c45b24920f5d792537
Joe-Lindie <josephlindie@hotmail.co.uk>
2023-10-29 15:18:29 +0000

    grid component created

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,13 +1,18 @@
 import Nav from '@/components/Nav';
+import Grid from '@/components/Grid';
 
 const About = () => {
   return (
     <>
       <Nav />
-      <main className="flex min-h-screen flex-col items-center">
+      <main className="flex flex-col items-center">
         <h1 className="font-bold">About Page</h1>
-        <p>some information</p>
+        <p className="pb-4">some information</p>
       </main>
+
+      <Grid>
+        <div className="col-start-1 col-end-13"></div>
+      </Grid>
     </>
   );
 };
---
+++ src/components/Grid.js
@@ -0,0 +1,5 @@
+const Grid = ({ children }) => {
+  return <div className="grid grid-cols-12 gap-4 px-20">{children}</div>;
+};
+
+export default Grid;
