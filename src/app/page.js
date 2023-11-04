7dbd4fb1b506f17ba3e252eb31a5761f3d784fdb
Joe-Lindie <josephlindie@hotmail.co.uk>
2023-11-04 14:46:02 +0000

    updates to topnav

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import HomepageCarousel from '@/components/HomepageCarousel';
 
 const Home = () => {
   return (
-    <div className="bg-lightBrown">
+    <div className="bg-lightBrown h-screen ">
       <TopNav />
       <main className="flex flex-col items-center">
         <h1 className="font-bold">Bindus Photography</h1>
--- src/components/coreUl/TopNav.js
+++ src/components/coreUl/TopNav.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
+import hamburgerMenu from '../../../public/hamburgerMenu.svg';
 
 const TopNav = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -9,14 +11,17 @@ const TopNav = () => {
   };
 
   return (
-    <>
+    <div className="bg-brown py-6">
       {/* Mobile Menu */}
       <div
         className="flex justify-end cursor-pointer md:hidden relative"
         onClick={handleToggle}
       >
-        <div className="flex flex-col absolute">
-          <span>Mobile Menu Toggle</span>
+        <div className="w-6 h-6 mr-6">
+          <Image src={hamburgerMenu} alt="Hamburger Menu" />
+        </div>
+
+        <div className={`flex flex-col absolute mt-10 bg-brown ${!isOpen ? 'h-screen p-6' : ""}`}>
           <ul className={`text-right ${isOpen ? 'hidden' : 'block'}`}>
             <li className="hover:underline">
               <Link href="/">Home</Link>
@@ -29,8 +34,8 @@ const TopNav = () => {
       </div>
 
       {/* Desktop Menu */}
-      <div className="hidden md:block">
-        <ul className="flex justify-end p-3">
+      <div className="hidden md:block pr-10">
+        <ul className="flex justify-end">
           <li className="pr-3 hover:underline">
             <Link href="/">Home</Link>
           </li>
@@ -39,7 +44,7 @@ const TopNav = () => {
           </li>
         </ul>
       </div>
-    </>
+    </div>
   );
 };
 
