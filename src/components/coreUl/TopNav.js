503029619e2cd45edca12b4eba25c47f10349166
Joe-Lindie <josephlindie@hotmail.co.uk>
2023-11-06 20:37:50 +0000

    mobile nav fixes

--- src/components/coreUl/TopNav.js
+++ src/components/coreUl/TopNav.js
@@ -12,24 +12,42 @@ const TopNav = () => {
 
   return (
     <div className="bg-lightBrown py-6">
+      {/* Mobile Menu */}
       <div
         className="flex justify-end cursor-pointer relative"
         onClick={handleToggle}
       >
         <div className="w-6 h-6 mr-6 md:hidden">
-          <Image src={hamburgerMenu} alt="Hamburger Menu" />
+          <Image src={hamburgerMenu} alt="mobile menu button" />
         </div>
+        <nav
+          className={`flex flex-col absolute mt-10 bg-lightBrown ${
+            isOpen ? 'h-screen p-6 md:hidden' : ''
+          }`}
+        >
+          <ul
+            className={`text-right ${isOpen ? 'block' : 'hidden h-screen p-6'}`}
+          >
+            <li className="hover:underline">
+              <Link href="/">Home</Link>
+            </li>
+            <li className="hover:underline">
+              <Link href="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
 
-      <nav className={`${ !isOpen ? 'hidden md:block pr-10' : ""}`}>
-        <ul className={`${ !isOpen ? 'flex justify-end' : ""}`}>
-          <li className="pr-3 hover:underline">
-            <Link href="/">Home</Link>
-          </li>
-          <li className="hover:underline">
-            <Link href="/about">About</Link>
-          </li>
-        </ul>
-      </nav>
+        {/* Desktop Menu */}
+        <nav className="hidden md:block pr-10">
+          <ul className="flex justify-end">
+            <li className="pr-3 hover:underline">
+              <Link href="/">Home</Link>
+            </li>
+            <li className="hover:underline">
+              <Link href="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
       </div>
     </div>
   );
