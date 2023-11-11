aa1e5072a1adf35a2d16aac47d8f9bd0bdcfd3aa
Joe-Lindie <josephlindie@hotmail.co.uk>
2023-11-11 12:22:28 +0000

    favicon added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,12 @@
+import Head from 'next/head';
 import '../globals.css';
 
 export const metadata = {
   title: 'photography',
   description: 'Photography portfolio and blog',
+  icons: {
+    icon: '/favicon.png',
+  },
 };
 
 const RootLayout = ({ children }) => {
