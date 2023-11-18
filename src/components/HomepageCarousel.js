ca5ffe48bd482e6c0977577211f16c43a5edc983
Joe-Lindie <josephlindie@hotmail.co.uk>
2023-11-18 11:31:40 +0000

    basic transition added

--- src/components/HomepageCarousel.js
+++ src/components/HomepageCarousel.js
@@ -6,7 +6,7 @@ import leftChevron from '../../public/left-chevron.png';
 
 const HomepageCarousel = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
-
+  
   const handleNext = () => {
     setCurrentIndex((currentIndex + 1) % backgroundImages.length);
   };
@@ -21,8 +21,8 @@ const HomepageCarousel = () => {
     <>
       {backgroundImages.map((image, index) => (
         <Image
-          className={`w-full h-full object-cover object-center ${
-            index === currentIndex ? 'visible' : 'hidden'
+          className={`w-full h-full object-cover object-center absolute ${
+            index === currentIndex ? 'visible animate-slide-in-left' : 'invisible'
           }`}
           key={image.url}
           src={image.url}
@@ -33,11 +33,11 @@ const HomepageCarousel = () => {
         ></Image>
       ))}
 
-      <div className="flex justify-center items-center pt-4">
-        <button className="w-8 h-8 pr-1" onClick={handleNext}>
+      <div className="flex justify-between inset-x-0 absolute">
+        <button className="w-8 h-8 mt-2.5 ml-2.5" onClick={handleNext}>
           <Image src={leftChevron} alt="left arrow" />
         </button>
-        <button className="w-8 h-8 pl-1" onClick={handlePrevious}>
+        <button className="w-8 h-8 mt-2.5 mr-2.5" onClick={handlePrevious}>
           <Image src={rightChevron} alt="right arrow" />
         </button>
       </div>
