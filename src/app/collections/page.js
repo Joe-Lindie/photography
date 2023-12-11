66c71752a65596c9936042f05b249270a0817a1f
Joe-Lindie <josephlindie@hotmail.co.uk>
2023-12-11 21:06:27 +0000

    pointer event added

--- src/app/collections/page.js
+++ src/app/collections/page.js
@@ -25,7 +25,7 @@ const Collections = () => {
       <div className="mt-4">
         <Grid>
           {cards.map((card, index) => (
-            <div key={index} className="col-span-6 h-60 relative">
+            <div key={index} className="col-span-6 h-60 relative ">
               <Link href={`/collections/${card.route}`}>
                 <Image
                   src={card.cover}
@@ -34,7 +34,7 @@ const Collections = () => {
                   height={500}
                   className="object-cover w-full h-full blur-sm hover:blur-none"
                 ></Image>
-                <p className="absolute top-1/2 left-1/2 transform translate-x-[-50%] translate-y-[-50%] text-white font-bold text-center">
+                <p className="pointer-events-none absolute top-1/2 left-1/2 transform translate-x-[-50%] translate-y-[-50%] text-white font-bold text-center">
                   {card.location}
                 </p>
               </Link>
