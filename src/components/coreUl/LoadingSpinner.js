078a778fde891bccd1debe035218ad7e7fccace4
Joe-Lindie <josephlindie@hotmail.co.uk>
2023-12-24 11:43:51 +0000

    fragment deleted

--- src/components/coreUl/LoadingSpinner.js
+++ src/components/coreUl/LoadingSpinner.js
@@ -1,11 +1,9 @@
 const LoadingSpinner = () => {
   return (
-    <>
-      <div className="flex items-center justify-center h-screen">
-        <div className="animate-spin h-5 w-5 border-t-2 border-black-rgba border-solid rounded-full"></div>
-        <p className="ml-2">Loading...</p>
-      </div>
-    </>
+    <div className="flex items-center justify-center h-screen">
+      <div className="animate-spin h-5 w-5 border-t-2 border-black-rgba border-solid rounded-full"></div>
+      <p className="ml-2">Loading...</p>
+    </div>
   );
 };
 
