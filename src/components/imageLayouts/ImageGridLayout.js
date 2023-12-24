27831000929fa8b8c9dc9ba15122050adbc356c9
Joe-Lindie <josephlindie@hotmail.co.uk>
2023-12-24 13:06:25 +0000

    image model added

--- src/components/imageLayouts/ImageGridLayout.js
+++ src/components/imageLayouts/ImageGridLayout.js
@@ -7,10 +7,22 @@ import LoadingSpinner from '../coreUl/LoadingSpinner';
 
 const ImageGridLayout = ({ imageData, title, alt }) => {
   const [isLoading, setIsLoading] = useState(true);
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [imageAlt, setImageAlt] = useState('');
 
   useEffect(() => {
     setIsLoading(false);
   }, []);
+
+  const openImage = (imageUrl, imageAlt) => {
+    setSelectedImage(imageUrl);
+    setImageAlt(imageAlt);
+  };
+
+  const closeImage = () => {
+    setSelectedImage(null);
+    setImageAlt('');
+  };
 
   return (
     <>
@@ -34,11 +46,39 @@ const ImageGridLayout = ({ imageData, title, alt }) => {
       ) : (
         <Grid>
           {imageData.map((image) => (
-            <div key={image.url} className="col-span-6 md:col-span-4">
-              <Image src={image.url} alt={alt} width={500} height={500}></Image>
+            <div
+              key={image.url}
+              className="col-span-6 md:col-span-4 cursor-pointer"
+            >
+              <Image
+                src={image.url}
+                alt={alt}
+                width={500}
+                height={500}
+                onClick={() => openImage(image.url, image.alt)}
+              ></Image>
             </div>
           ))}
         </Grid>
+      )}
+
+      {selectedImage && (
+        <div className="z-30 w-full h-full flex items-center justify-center top-0 left-0 fixed bg-black bg-opacity-70 backdrop-blur-md">
+          <p
+            onClick={() => closeImage()}
+            className="text-white text-right cursor-pointer absolute top-0 right-0 pr-4 pt-4"
+          >
+            close
+          </p>
+          <Image
+            src={selectedImage}
+            alt={imageAlt}
+            priority
+            width={1000}
+            height={1000}
+            className="w-[45rem] h-[20rem] sm:h-[25rem] md:h-[28rem] object-cover"
+          ></Image>
+        </div>
       )}
     </>
   );
